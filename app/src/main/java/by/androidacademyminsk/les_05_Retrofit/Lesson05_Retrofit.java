4355faa332d29b96f6a6c44a654657ea1b569a8c
d3ibah <buae@ya.ru>
2019-03-13 23:49:11 +0300

    Add UI drafts

--- app/src/main/java/by/androidacademyminsk/les_05_Retrofit/Lesson05_Retrofit.java
+++ app/src/main/java/by/androidacademyminsk/les_05_Retrofit/Lesson05_Retrofit.java
@@ -1,15 +1,40 @@
 package by.androidacademyminsk.les_05_Retrofit;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import by.androidacademyminsk.R;
 
 public class Lesson05_Retrofit extends AppCompatActivity {
 
+    private Button searchAction;
+    private EditText keywordSource;
+    private String keyword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lesson05__retrofit);
+        setContentView(R.layout.activity_lesson05_retrofit);
+
+        searchAction = findViewById(R.id.les05_button);
+        keywordSource = findViewById(R.id.les05_editText);
+
+        searchAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInputSymbols();
+                if (TextUtils.isEmpty(keyword)) {
+//                    TODO: Waiting implementation
+                }
+            }
+        });
+    }
+
+    private void getInputSymbols() {
+        keyword = keywordSource.getText().toString().trim();
     }
 }

3bfa5f4b9ff2ba3245d79f5c2bb2c53265597d4d
alex.dzervanouski <alex.dzervanouski@exposit.com>
2019-03-14 18:02:55 +0300

    Update network API

--- app/src/main/java/by/androidacademyminsk/les_05_Retrofit/FilmsAPI.java
+++ app/src/main/java/by/androidacademyminsk/les_05_Retrofit/FilmsAPI.java
@@ -1,5 +1,6 @@
 package by.androidacademyminsk.les_05_Retrofit;
 
+import by.androidacademyminsk.les_05_Retrofit.entity.Film.Film;
 import by.androidacademyminsk.les_05_Retrofit.entity.films.Films;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,4 +10,7 @@ public interface FilmsAPI {
 
     @GET("/")
     Call<Films> getFilms(@Query("apikey") String apikey, @Query("s") String keyword);
+
+    @GET("/")
+    Call<Film> getFilm(@Query("apikey") String apikey, @Query("t") String filmName);
 }
